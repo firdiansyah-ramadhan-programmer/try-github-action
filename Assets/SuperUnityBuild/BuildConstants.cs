fbe653781194487a4cc325dec53c2f7c86df78ac
Firdiansyah TUF <firdiansyah@projectseed.studio>
2022-04-29 09:38:30 +0700

    add super unity build

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,35 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Built_Automation,
+    }
+
+    public enum Platform
+    {
+        None,
+        Android,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Android,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637868204905290837);
+    public const string version = "1.0.0.1";
+    public const ReleaseType releaseType = ReleaseType.Built_Automation;
+    public const Platform platform = Platform.Android;
+    public const Architecture architecture = Architecture.Android;
+    public const Distribution distribution = Distribution.None;
+}
+
